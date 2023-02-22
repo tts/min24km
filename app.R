96a6b98d8d36f3791fa75af793e674b421cd0324
Tuija <tuija.sonkkila@aalto.fi>
2023-02-22 23:05:10 +0200

    Changed link

--- app.R
+++ app.R
@@ -8,7 +8,7 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%} .leaflet-container {cursor: pointer !important;}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(bottom = 40, right = 20,
-                HTML("<div><b>Click anywhere</b><br/><a target='blank' href='https://www.sciencedirect.com/science/article/pii/S2214140522002079'>About</a></div>")
+                HTML("<div><b>Click anywhere</b><br/><a target='blank' href='https://www.hs.fi/tiede/art-2000009337638.html?fbclid=IwAR1AlfLLDmGWmFteOWp6-reI5cu36_SE87P-Zf5Jbi0-JuFsuXMsI_aPom0'>About</a></div>")
   )
 )
 
